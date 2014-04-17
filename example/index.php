bdc8cb672a5f06b5ce010f7bbd970463e535304c
PierreGaudichon <Umbra.sf@gmail.com>
2014-04-17 13:04:05 +0200

    Comment the example file.

--- example/index.php
+++ example/index.php
@@ -1,9 +1,14 @@
 <?php
 
+// Include all the dependencies
+// run the `dependencies` file first.
+
 require_once '../flight/flight/Flight.php';
 require_once '../sparrow/sparrow.php';
 require_once "../hzs.php";
 
+
+// Set the database. You need to have access to a MySQL database to use it.
 
 /*
 Hzs::setDb(array(
@@ -15,12 +20,29 @@ Hzs::setDb(array(
 */
 
 
+// Main route, just a small "Hello world !!"
+
 Hzs::route("/", array(), function($db, $data, $res){
 
 	$res["hello"] = "world";
 
 	return $res;
 });
+
+
+// Another example route, an echo server.
+
+Hzs::route("/echo", array(
+	"name"
+	), function($db, $data, $res){
+
+		$res["hi"] = $data["name"];
+
+		return $res;
+});
+
+
+// Start the routing.
 
 Hzs::start();
 
